f8b5795f248ec1f62b607d8a20de39618cb078ab
RKAbdul <64411354+RKAbdul@users.noreply.github.com>
2020-07-27 20:51:01 +0200

    Add files via upload

---
+++ src/RKAbdul/GimmeHead/Main.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace RKAbdul\GimmeHead;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\Server;
+
+class Main extends PluginBase {
+	public function onEnable(){
+		$this->saveDefaultConfig();
+		$this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+	}
+}
