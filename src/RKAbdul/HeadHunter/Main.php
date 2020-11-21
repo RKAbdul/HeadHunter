295b3818b5f95520639d0db877dfb32902ebbf31
RKAbdul <64411354+RKAbdul@users.noreply.github.com>
2020-11-21 21:52:16 +0100

    Added config version check!

--- src/RKAbdul/HeadHunter/Main.php
+++ src/RKAbdul/HeadHunter/Main.php
@@ -7,8 +7,18 @@ use pocketmine\utils\Config;
 use pocketmine\Server;
 
 class Main extends PluginBase {
+
+        public const VERSION = 2;
+
+        private $cfg;
+
 	public function onEnable(){
 		$this->saveDefaultConfig();
+                $this->cfg = $this->getConfig()->getAll();
+                if ($this->cfg["version"] < self::VERSION) {
+                    $this->getLogger()->error("Config Version is outdated! Please delete your current config file!");
+                    $this->getServer()->getPluginManager()->disablePlugin($this);
+                }
 		$this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
 	}
 }
